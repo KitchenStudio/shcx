5e995b40c53a9407da1f6563a3dcc2b110dfd92b
airkid <yanhaoairoi@126.com>
2015-01-28 23:05:58 +0800

    Change the Name of DriverRespository

--- shcx/src/main/java/org/kitchenstudio/service/DriverServiceImpl.java
+++ shcx/src/main/java/org/kitchenstudio/service/DriverServiceImpl.java
@@ -3,33 +3,33 @@ package org.kitchenstudio.service;
 import java.util.List;
 
 import org.kitchenstudio.model.Driver;
-import org.kitchenstudio.repository.DriverRespository;
+import org.kitchenstudio.repository.DriverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DriverServiceImpl implements DriverService{
 
-	private final DriverRespository driverRespository;
+	private final DriverRepository driverRepository;
 	
 	@Autowired
-	public DriverServiceImpl(DriverRespository driverRespository){
-		this.driverRespository = driverRespository;
+	public DriverServiceImpl(DriverRepository driverRespository){
+		this.driverRepository = driverRespository;
 	}
 	
 	@Override
 	public List<Driver> findAll() {
-		return driverRespository.findAll();
+		return driverRepository.findAll();
 	}
 
 	@Override
 	public void add(Driver driver) {
-		driverRespository.save(driver);
+		driverRepository.save(driver);
 	}
 
 	@Override
 	public void delete(Driver driver) {
-		driverRespository.delete(driver);
+		driverRepository.delete(driver);
 	}
 
 }

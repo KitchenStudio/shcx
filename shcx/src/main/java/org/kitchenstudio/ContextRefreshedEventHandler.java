50e5c2850460499e1b471b05e0da03fc7841a1f0
sealday <sealday@gmail.com>
2015-02-04 23:57:55 +0800

    去除仓库类型

--- shcx/src/main/java/org/kitchenstudio/ContextRefreshedEventHandler.java
+++ shcx/src/main/java/org/kitchenstudio/ContextRefreshedEventHandler.java
@@ -7,9 +7,6 @@ import org.kitchenstudio.entity.Product;
 import org.kitchenstudio.entity.ProductCategory;
 import org.kitchenstudio.entity.ProductSpecification;
 import org.kitchenstudio.entity.Staff;
-import org.kitchenstudio.entity.StoreItem;
-import org.kitchenstudio.entity.StoreType;
-import org.kitchenstudio.entity.Type;
 import org.kitchenstudio.repository.CompanyRepository;
 import org.kitchenstudio.repository.ContractRepository;
 import org.kitchenstudio.repository.DriverRepository;
@@ -17,7 +14,6 @@ import org.kitchenstudio.repository.ProductCategoryRepository;
 import org.kitchenstudio.repository.ProductRepository;
 import org.kitchenstudio.repository.ProductSpecificationRepository;
 import org.kitchenstudio.repository.StaffRepository;
-import org.kitchenstudio.repository.StoreRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +34,6 @@ public class ContextRefreshedEventHandler implements
 	@Autowired
 	private DriverRepository driverRepository;
 
-	@Autowired
-	private StoreRepository storeRepository;
 
 	@Autowired
 	private ContractRepository contractRepository;
@@ -118,21 +112,6 @@ public class ContextRefreshedEventHandler implements
 
 		// 合同初始化
 		contractRepository.save(new Contract());
-
-		// 仓库初始化
-		StoreItem storeItem = new StoreItem();
-		storeItem.setStoreType(new StoreType(Type.STEEL_PIPE, "1.1"));
-		storeItem.setQuantity(10);
-		storeRepository.save(storeItem);
-
-		storeItem = new StoreItem();
-		storeItem.setStoreType(new StoreType(Type.STEEL_PIPE, "1.1"));
-		storeItem.setQuantity(21);
-		storeRepository.save(storeItem);
-
-		storeItem = storeRepository.findOne(new StoreType(Type.STEEL_PIPE,
-				"1.1"));
-		logger.info(String.format("钢管1.1米有 --- %d 米", storeItem.getQuantity()));
 
 	}
 }
--- shcx/src/main/java/org/kitchenstudio/repository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package org.kitchenstudio.repository;
-
-import org.kitchenstudio.entity.StoreItem;
-import org.kitchenstudio.entity.StoreType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<StoreItem, StoreType> {
-
-}

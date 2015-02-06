eaec522d741e1d080ac221182c72bb522c468f22
sealday <sealday@gmail.com>
2015-02-06 09:39:59 +0800

    采购

--- shcx/src/main/java/org/kitchenstudio/entity/PurchaseItem.java
+++ shcx/src/main/java/org/kitchenstudio/entity/PurchaseItem.java
@@ -1,15 +1,12 @@
 package org.kitchenstudio.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.Min;
 
-import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class PurchaseItem {
@@ -28,8 +25,6 @@ public class PurchaseItem {
 
 	@Min(value = 0, message = "数量不能为负")
 	private int quantity;
-
-	private String remark;
 
 	public Long getId() {
 		return id;
@@ -61,14 +56,6 @@ public class PurchaseItem {
 
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
-	}
-
-	public String getRemark() {
-		return remark;
-	}
-
-	public void setRemark(String remark) {
-		this.remark = remark;
 	}
 
 }

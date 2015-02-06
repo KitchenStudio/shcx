f040c7711e107bc4c4db6c65951dce4202501dd7
sealday <sealday@gmail.com>
2015-02-06 08:32:44 +0800

    去掉site中的输出

--- shcx/src/main/java/org/kitchenstudio/web/controller/SiteController.java
+++ shcx/src/main/java/org/kitchenstudio/web/controller/SiteController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.kitchenstudio.entity.Company;
-import org.kitchenstudio.entity.Order;
 import org.kitchenstudio.entity.Site;
 import org.kitchenstudio.entity.SiteType;
 import org.kitchenstudio.service.CompanyService;
@@ -19,78 +18,77 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 @RequestMapping("/site")
 public class SiteController {
-	
+
 	@Autowired
 	SiteService siteService;
-	
+
 	@Autowired
 	CompanyService companyService;
-	
+
 	@ModelAttribute("siteTypes")
-	public SiteType [] populateSites(){
+	public SiteType[] populateSites() {
 		return SiteType.values();
 	}
-	
+
 	@ModelAttribute("companies")
 	public List<Company> populateStaffs() {
-		System.out.println(companyService.findAll().size()+"size");
 		return companyService.findAll();
 	}
-	
-	@RequestMapping({"","/"})
-	String home(Model model){
+
+	@RequestMapping({ "", "/" })
+	String home(Model model) {
 		List<Site> sites = siteService.findAll();
 		model.addAttribute("sites", sites);
 		return "site/home";
 	}
-	
-	@RequestMapping(value="/new",method=RequestMethod.GET)
-	String create(Model model){
-		if(!model.containsAttribute("site")){
+
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	String create(Model model) {
+		if (!model.containsAttribute("site")) {
 			Site site = new Site();
 			model.addAttribute(site);
 		}
 		return "site/new";
-		
+
 	}
-	
-	@RequestMapping(value="/new",method=RequestMethod.POST)
-	String create(@Valid Site site,BindingResult result){
-		if(result.hasErrors()){
-			return"site/new";
+
+	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	String create(@Valid Site site, BindingResult result) {
+		if (result.hasErrors()) {
+			return "site/new";
 		}
 		siteService.add(site);
 		return "redirect:/site";
 	}
-	
-	@RequestMapping(value="/{id}/detail",method=RequestMethod.GET)
-	String detail(@PathVariable("id")Site site,Model model){
+
+	@RequestMapping(value = "/{id}/detail", method = RequestMethod.GET)
+	String detail(@PathVariable("id") Site site, Model model) {
 		model.addAttribute(site);
 		return "site/detail";
 	}
-	
-	@RequestMapping(value="/{id}/modify",method=RequestMethod.GET)
-	String modify(@PathVariable("id")Site site,Model model){
+
+	@RequestMapping(value = "/{id}/modify", method = RequestMethod.GET)
+	String modify(@PathVariable("id") Site site, Model model) {
 		model.addAttribute(site);
 		return "site/modify";
 	}
-	@RequestMapping(value="/{id}/modify",method=RequestMethod.POST)
-	String modify(@Valid Site site,BindingResult result){
-		if(result.hasErrors()){
+
+	@RequestMapping(value = "/{id}/modify", method = RequestMethod.POST)
+	String modify(@Valid Site site, BindingResult result) {
+		if (result.hasErrors()) {
 			return "site/modify";
 		}
 		siteService.add(site);
 		return "redirect:/site/{id}/detail?success";
 	}
-	
+
 	@RequestMapping("/{id}/delete")
 	String delete(@PathVariable("id") Site site) {
 		siteService.delete(site);
 		return "redirect:/site";
 	}
-	
+
 }

3960e4e3338e4ce4051f6b754c9c25f402087864
sealday <sealday@gmail.com>
2015-02-21 14:44:54 +0800

    修正了data.sql中的语法不兼容（hsqldb与mysql)

--- shcx/src/main/resources/data.sql
+++ shcx/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `company`.`staff` (`idcard`, `bank_number`, `gender`, `name`, `phone_number`) VALUES ('450322199203140552', '1234123412341234123', 'MALE', '林', '18366110000');
+INSERT INTO staff (idcard, bank_number, gender, name, phone_number) VALUES ('450322199203140552', '1234123412341234123', 'MALE', '林', '18366110000');
